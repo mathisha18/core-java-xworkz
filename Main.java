5935ea26dcffa1c02dfa54d8ce11a12786dfedf5
mathisha18 <arumugammathisha@gmail.com>
2023-07-20 09:54:00 +0530

    adding Presentation-Task files

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+static int age[] = {12, 3, 5, 9};
+public static void main(String args[]){
+System.out.println("Using loop");
+for (int i=0; i<age.length; i++){
+System.out.println(age[i]);
+}
+}
+}
